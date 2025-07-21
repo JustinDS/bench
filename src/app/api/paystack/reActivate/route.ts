0d2aa49284d17acb8f99f3df14fc789b4e6c4f07
Justin <justin1991schmidt@gmail.com>
2025-07-21 22:10:46 +0200

    add some console logs to see what is happening

--- src/app/api/paystack/reActivate/route.ts
+++ src/app/api/paystack/reActivate/route.ts
@@ -24,28 +24,39 @@ export async function POST(req: NextRequest) {
     );
   }
 
-  const res = await fetch("https://api.paystack.co/subscription/enable", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      code: profile.subscription_code,
-      token: profile.email_token,
-    }),
-  });
+  console.log("profile reactivate", profile);
 
-  const result = await res.json();
+  try {
+    const res = await fetch("https://api.paystack.co/subscription/enable", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code: profile.subscription_code,
+        token: profile.email_token,
+      }),
+    });
 
-  if (!res.ok) {
-    return NextResponse.json({ error: result.message }, { status: res.status });
+    const result = await res.json();
+
+    console.log("result", result);
+
+    if (!res.ok) {
+      return NextResponse.json(
+        { error: result.message },
+        { status: res.status }
+      );
+    }
+
+    await (await supabase)
+      .from("profiles")
+      .update({ subscription_status: "active" })
+      .eq("id", user.id);
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error(error);
   }
-
-  await (await supabase)
-    .from("profiles")
-    .update({ subscription_status: "active" })
-    .eq("id", user.id);
-
-  return NextResponse.json({ success: true });
 }
